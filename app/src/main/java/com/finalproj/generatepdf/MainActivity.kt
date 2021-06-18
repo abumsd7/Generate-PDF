5d0f4a51c8eb4904809cb215ee2fb04ea4eccde4
Abu Murugesan <abumurugesan@gmail.com>
2021-06-18 20:33:17 +0530

    Some Corrections

--- app/src/main/java/com/finalproj/generatepdf/MainActivity.kt
+++ app/src/main/java/com/finalproj/generatepdf/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.finalproj.generatepdf
 
 import android.Manifest
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.database.Cursor
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
@@ -16,7 +18,9 @@ import androidx.core.content.ContextCompat
 import com.itextpdf.text.Document
 import com.itextpdf.text.Image
 import com.itextpdf.text.pdf.PdfWriter
+import java.io.File
 import java.io.FileOutputStream
+import java.net.URLDecoder
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
@@ -47,10 +51,22 @@ class MainActivity : AppCompatActivity() {
         val directoryPath = Environment.getExternalStorageDirectory().toString()
         val date = getCurrentDateTime()
         val dateInString = date.toString("yyyy_MM_dd_HH_mm_ss")
+        val f = File( directoryPath ,"IndexedPDFs")
+        f.mkdir()
         PdfWriter.getInstance(document, FileOutputStream("$directoryPath/IndexedPDFs/$dateInString.pdf")) //  Change pdf's name.
         document.open()
         for (uris in uriList) {
-            val image: Image = Image.getInstance(getRealPathFromURI(this, uris)) // Change image's name and extension.
+            Log.i("Uris", uris.toString())
+            var realPath : String?
+            if(uris.toString().contains("com.simplemobiletools.gallery.pro.provider/external_files/"))
+            {
+                realPath = uris.toString().removeRange(0,68)
+                realPath = Uri.decode(realPath)
+                Log.i("Realpaths",realPath)
+            }
+            else realPath = getRealPathFromURI(this,uris)
+            Log.i("Realpaths(converted)",realPath)
+            val image: Image = Image.getInstance(realPath) // Change image's name and extension.
             val scaler: Float = (document.pageSize.width - document.leftMargin()
                     - document.rightMargin() - 0) / image.width * 100 // 0 means you have no indentation. If you have any, change it.
 
@@ -111,6 +127,43 @@ class MainActivity : AppCompatActivity() {
             cursor!!.getString(columnIndex)
         } finally {
             cursor?.close()
+        }
+    }
+
+    fun getImageContentUri(context: Context, imageFile: File): Uri? {
+        val filePath = imageFile.absolutePath
+        val cursor = context.contentResolver.query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, arrayOf(MediaStore.Images.Media._ID),
+                MediaStore.Images.Media.DATA + "=? ", arrayOf(filePath), null)
+        return if (cursor != null && cursor.moveToFirst()) {
+            val id = cursor.getInt(cursor.getColumnIndex(MediaStore.MediaColumns._ID))
+            cursor.close()
+            Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "" + id)
+        } else {
+            if (imageFile.exists()) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    val resolver = context.contentResolver
+                    val picCollection = MediaStore.Images.Media
+                            .getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+                    val picDetail = ContentValues()
+                    picDetail.put(MediaStore.Images.Media.DISPLAY_NAME, imageFile.name)
+                    picDetail.put(MediaStore.Images.Media.MIME_TYPE, "image/jpg")
+                    picDetail.put(MediaStore.Images.Media.RELATIVE_PATH, "DCIM/" + UUID.randomUUID().toString())
+                    picDetail.put(MediaStore.Images.Media.IS_PENDING, 1)
+                    val finaluri = resolver.insert(picCollection, picDetail)
+                    picDetail.clear()
+                    picDetail.put(MediaStore.Images.Media.IS_PENDING, 0)
+                    resolver.update(picCollection, picDetail, null, null)
+                    finaluri
+                } else {
+                    val values = ContentValues()
+                    values.put(MediaStore.Images.Media.DATA, filePath)
+                    context.contentResolver.insert(
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+                }
+            } else {
+                null
+            }
         }
     }
 }
